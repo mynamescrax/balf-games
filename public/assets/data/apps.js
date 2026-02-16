54ce0806797b4fbe1dc03441d0f521eb6a0b49c2
imm1styz <132205740+Louct@users.noreply.github.com>
2026-02-16 20:04:44 +0000

    Fix formatting issues in apps.js

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -76,7 +76,7 @@ var apps = [
     {
       'id': 'spenflix',
       'title': 'Spenflix',
-      'url': 'https://https://frogiesarcade.win/embed.html#https://watch.spencerdevs.xyz',
+      'url': 'https://watch.spencerdevs.xyz',
       'image': 'https://frogiesarcade.win/stuff/apps/movies.jpg'
     },
   ]
