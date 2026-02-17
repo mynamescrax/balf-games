f7d6580187b7955e1836da00dc4305aa35d680bb
imm1styz <132205740+Louct@users.noreply.github.com>
2026-02-17 18:14:47 +0000

    Add files via upload

---
+++ public/assets/data/apps.js
@@ -0,0 +1,85 @@
+var apps = [
+    {
+      'id': 'customapp',
+      'title': 'Add custom app',
+      'image': 'assets/images/add.png'
+    },
+    {
+      "id": "google",
+      "title": "Google",
+      "url": "https://www.google.com/?safe=active&ssui=on",
+      'image': 'https://img.freepik.com/free-icon/search_318-265146.jpg'
+      
+    },
+    {
+      "id": "discord",
+      "title": "Discord",
+      "description": "most likely wont work unless discord updates their website and it suddenly works",
+      "url": "https://discord.com/app",
+      'image': 'https://static.vecteezy.com/system/resources/previews/006/892/625/original/discord-logo-icon-editorial-free-vector.jpg'
+    },
+    {
+      "id": "telegram",
+      "title": "Telegram",
+      "url": "https://web.telegram.org/a/",
+      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png'
+    }, 
+    {
+      'id': 'vscode',
+      'title': 'Visual Studio Code',
+      'url': 'https://vscode.dev/',
+      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png'
+    },
+    {
+      "id": "tiktok",
+      "title": "TikTok",
+      "url": "https://tiktok.com",
+      'image': 'https://sf-tb-sg.ibytedtos.com/obj/eden-sg/uhtyvueh7nulogpoguhm/tiktok-icon2.png'
+    },
+    {
+      "id": "reddit",
+      "title": "Reddit",
+      "url": "https://reddit.com",
+      'image': 'https://static.vecteezy.com/system/resources/previews/018/930/582/non_2x/reddit-logo-reddit-icon-transparent-free-png.png'
+    },
+    {
+      "id": "youtube",
+      "title": "YouTube",
+      "url": "https://youtube.com",
+      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/YouTube_social_red_square_%282017%29.svg/2048px-YouTube_social_red_square_%282017%29.svg.png'
+    },
+    {
+      'id': 'spotify',
+      'title': 'Spotify',
+      'url': 'https://open.spotify.com/browse',
+      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png'
+    },
+    {
+      'id': 'chess',
+      'title': 'Chess.com',
+      'url': 'https://chess.com',
+      'image': 'https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/SamCopeland/phpmeXx6V.png'
+    },
+    {
+      'id': 'palyra',
+      'title': 'Palyra (Discord alternative)',
+      'description': 'Please dont have any personal information or use a personal password on this website',
+      'url': 'https://palyra.vercel.app',
+      'image': 'https://palyra.vercel.app/images/icon.png'
+    },
+    {
+       id: "spenflix",
+       title: "Spenflix",
+       description: "mid movie player",
+       url: "https://velara.cc/scramjet/https%3A%2F%2Fwatch.spencerdevs.xyz%2F",
+       image: "https://frogiesarcade.win/stuff/apps/movies.jpg"
+  }
+  ]
+
+  var customapps = JSON.parse(localStorage.getItem("customapps"))
+  if (customapps) customapps.forEach(app => {
+    apps.push(app)
+  })
+  
+  console.log("Loaded " + apps.length + " apps")
+  if (customapps) console.log("Loaded " + customapps.length + " custom apps")
