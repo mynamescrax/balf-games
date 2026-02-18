9150cb6b426cb37b1d0b3211e64c582dc7ee708c
crax <132205740+mynamescrax@users.noreply.github.com>
2026-02-18 16:27:48 +0000

    Update print statement from 'Hello' to 'Goodbye'

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -44,7 +44,7 @@ var games = [
     id: 'veckio',
     title: 'Veck.io',
     description: 'Veck.io is a 3D multiplayer FPS IO shooting game with nonstop action among players across the globe. Players can compete in arcade mode, team battles ranging from 1v1 to 4v4, or fast-paced Gun Game challenges.',
-    url: 'https://www.play-games.com/iframe/39726.html',
+    url: 'https://agaronline.io/veck-io.embed',
     image: 'https://hollowknight.io/data/image/game/veck-io/veck-io.png'
   },
   {
@@ -366,5 +366,6 @@ if (customgames) customgames.forEach(game => {
 
 console.log("Loaded " + games.length + " games")
 if (customgames) console.log("Loaded " + customgames.length + " custom games")
+
 
 
